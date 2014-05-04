766ae45c26bfffec0818a6c8512866107a363a78
Sean Hurley <sean.hurley6@gmail.com>
2014-05-04 14:59:51 -0500

    Ruby - Day 1

---
+++ ruby/day_1.rb
@@ -0,0 +1,8 @@
+puts "Hello, world!"
+
+index = "Hello, Ruby,".index("Ruby")
+puts "Index of ruby at #{index}"
+
+10.times { puts "Hurley" }
+
+10.times { |i| puts "This is sentence number #{i+1}" }
