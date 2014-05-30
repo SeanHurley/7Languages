8a7f13f673caf7f3b0acbb820cb305d598d16e28
Sean Hurley <sean.hurley6@gmail.com>
2014-05-30 14:29:48 -0500

    Prolog - Day 1

---
+++ prolog/day1.pl
@@ -0,0 +1,17 @@
+musician(jimihendrix).
+musician(albertking).
+musician(bbking).
+musician(robertjohnson).
+
+musician(stevewhite).
+musician(stephenmorris).
+musician(jeffpocaro).
+
+plays_instrument(jimihendrix, guitar).
+plays_instrument(albertking, guitar).
+plays_instrument(bbking, guitar).
+plays_instrument(robertjohnson, guitar).
+
+plays_instrument(stevewhite, drums).
+plays_instrument(stephenmorris, drums).
+plays_instrument(jeffpocaro, drums).
